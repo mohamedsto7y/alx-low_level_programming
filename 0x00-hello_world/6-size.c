efeefe94df595f563e22535e6637dd2301f21242
mohamedsto7y <mohamed_alsayed@live.com>
2023-06-15 13:48:07 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description using sizeof to print the size of various types.
+ *
+ * Return: always 0 (Sucess)
+*/
+int main (void)
+{
+	printf("Size of a char: %u byte(s)\n	
